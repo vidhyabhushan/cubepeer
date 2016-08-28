067b1c54775fc8e027aad6e0bc2957f433caaa0c
vidhyabhushan <vidverma.cubepeer@gmail.com>
2016-08-29 03:24:38 +0530

    Added single page web support and webpage routing.
    Issues:
    1. map is not working
    2. form is not working
    3. content modification required
    4. footer is not stick to the bottom

--- WEB_CUBEPEER_DEV_1/js/info.js
+++ WEB_CUBEPEER_DEV_1/js/info.js
@@ -1,36 +1,31 @@
-jQuery(document).ready(function($){
-//$('#form-container').submit(saveData);
+var cpApp = angular.module('cubepeer', ['ngRoute']);
+
+cpApp.directive('cubepeerFooter', function(){
+    return {
+        restrict: 'E',
+        templateUrl: 'cubepeer-footer.html'
+    };
 });
 
 
-
-function saveData() {
-    
-    function loadData() {
-        
-
-        var $requestInfo = $('#requestInfo');
- 
-        // load request information data
-        var info = new Object();
-        info.firstName = $('#firstName').val();
-        info.companyName = $('#companyName').val();
-        info.mobileNumber = $('#mobileNumber').val();
-        info.emailAddress = $('#emailAddress').val();
-        info.request = $('#request').val();
-
-        
-        // YOUR CODE GOES HERE!
-        console.log(info.firstName +" "+ info.companyName+" "+"Contact Vidverma ");
-        info.resetInfo = function() {
-            $('#form-container').reset();
-        };
-        //info.resetInfo();
-        grecaptcha.reset();
-        return false;
-    }
-    
-    loadData();
-    return false;
-}
-
+cpApp.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "cubepeer-main.html"
+    })
+    .when("/services", {
+        templateUrl : "services.html"
+    })
+    .when("/careers", {
+        templateUrl : "careers.html"
+    })
+    .when("/about", {
+        templateUrl : "about.html"
+    })
+    .when("/contacts", {
+        templateUrl : "contacts.html"
+    })
+    .otherwise({
+        redirectTo:'/'
+    });
+});
